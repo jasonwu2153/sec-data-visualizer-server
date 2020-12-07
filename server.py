bfec9f1be6fc6d8b9a8d15313b18dd84deb4ad30
Jason Wu <jason.wu2153@gmail.com>
2020-12-07 00:02:58 -0500

    added get request code

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 
 from db import cnx
+from sql import get_sec_companies_sql
 
 app = Flask(__name__)
 
 @app.route('/sec-companies', methods=['GET'])
 def get_sec_companies():
     'Returns sec-companies from the database in alphabetical order.'
-    pass
+    cursor = cnx.cursor()
+    cursor.execute(get_sec_companies_sql)
+
+    desc = cursor.description
+    attr_names = [attr[0] for attr in desc]
+    data = [dict(zip(attr_names, row))  
+        for row in cursor.fetchall()]
+
+    cursor.close()
+    return jsonify(data)
---
+++ sql.py
@@ -0,0 +1,1 @@
+get_sec_companies_sql = 'SELECT * FROM sec_companies ORDER BY name ASC'

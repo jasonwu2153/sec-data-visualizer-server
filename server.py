cb09eb782c067b6939959337f882f0768d6ee15f
Jason Wu <jason.wu2153@gmail.com>
2020-12-08 23:35:46 -0500

    added top ten by number of shares

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 
 from db import cnx
-from sql import get_sec_companies_sql, get_top_ten_holdings
+from sql import get_sec_companies_sql, get_top_ten_holdings_by_value, get_top_ten_holdings_by_number_shares
 
 app = Flask(__name__)
 
@@ -17,20 +17,33 @@ def get_sec_companies():
         for row in cursor.fetchall()]
 
     cursor.close()
-    return jsonify(data)
+    return jsonify({ 'data': data })
 
 @app.route('/holdings-data', methods=['GET'])
 def get_holdings_data():
     'Returns holding data from the database for the company with the requested cik.'
     cik = request.args.get('cik')
     cursor = cnx.cursor()
-    cursor.execute(get_top_ten_holdings, (cik,))
 
+    # top ten by value
+    cursor.execute(get_top_ten_holdings_by_value, (cik,))
     desc = cursor.description
     attr_names = [attr[0] for attr in desc]
-    top_ten_data = [dict(zip(attr_names, row))  
+    top_ten_by_value_data = [dict(zip(attr_names, row))  
         for row in cursor.fetchall()]
 
+    # top ten by number of shares
+    cursor.execute(get_top_ten_holdings_by_number_shares, (cik,))
+    desc = cursor.description
+    attr_names = [attr[0] for attr in desc]
+    top_ten_by_number_shares_data = [dict(zip(attr_names, row))  
+        for row in cursor.fetchall()]
+
+    data = {
+        'top_ten_by_value': top_ten_by_value_data,
+        'top_ten_by_number_shares': top_ten_by_number_shares_data
+    }
+
     cursor.close()
-    return jsonify(top_ten_data)
+    return jsonify({ 'data' : data })
 
--- sql.py
+++ sql.py
@@ -1,14 +1,27 @@
 get_sec_companies_sql = 'SELECT * FROM sec_companies ORDER BY name ASC;'
-get_top_ten_holdings = '''SELECT holdings.lei, 
-                             legal_entities.name,
-                             legal_entities.title,
-                             holdings.isin,
-                             holdings.cusip,
-                             holdings.units,
-                             holdings.balance,
-                             holdings.val_usd
-                       FROM holdings
-                       INNER JOIN legal_entities ON holdings.lei = legal_entities.lei 
-                       WHERE held_by = %s
-                       ORDER BY val_usd DESC
-                       LIMIT 10;'''
+get_top_ten_holdings_by_value = '''SELECT holdings.lei, 
+                                          legal_entities.name,
+                                          legal_entities.title,
+                                          holdings.isin,
+                                          holdings.cusip,
+                                          holdings.units,
+                                          holdings.balance,
+                                          holdings.val_usd
+                                    FROM holdings
+                                    INNER JOIN legal_entities ON holdings.lei = legal_entities.lei 
+                                    WHERE held_by = %s AND units = 'NS'
+                                    ORDER BY val_usd DESC
+                                    LIMIT 10;'''
+get_top_ten_holdings_by_number_shares = '''SELECT holdings.lei, 
+                                          legal_entities.name,
+                                          legal_entities.title,
+                                          holdings.isin,
+                                          holdings.cusip,
+                                          holdings.units,
+                                          holdings.balance,
+                                          holdings.val_usd
+                                    FROM holdings
+                                    INNER JOIN legal_entities ON holdings.lei = legal_entities.lei 
+                                    WHERE held_by = %s AND units = 'NS'
+                                    ORDER BY balance DESC
+                                    LIMIT 10;'''

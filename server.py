220fd971e0a2ef42bdd5926b70960f579a4f4a12
Jason Wu <jason.wu2153@gmail.com>
2020-12-08 23:27:30 -0500

    added top 10 data

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 
 from db import cnx
-from sql import get_sec_companies_sql
+from sql import get_sec_companies_sql, get_top_ten_holdings
 
 app = Flask(__name__)
 
@@ -18,3 +18,19 @@ def get_sec_companies():
 
     cursor.close()
     return jsonify(data)
+
+@app.route('/holdings-data', methods=['GET'])
+def get_holdings_data():
+    'Returns holding data from the database for the company with the requested cik.'
+    cik = request.args.get('cik')
+    cursor = cnx.cursor()
+    cursor.execute(get_top_ten_holdings, (cik,))
+
+    desc = cursor.description
+    attr_names = [attr[0] for attr in desc]
+    top_ten_data = [dict(zip(attr_names, row))  
+        for row in cursor.fetchall()]
+
+    cursor.close()
+    return jsonify(top_ten_data)
+
--- sql.py
+++ sql.py
@@ -1,1 +1,14 @@
-get_sec_companies_sql = 'SELECT * FROM sec_companies ORDER BY name ASC'
+get_sec_companies_sql = 'SELECT * FROM sec_companies ORDER BY name ASC;'
+get_top_ten_holdings = '''SELECT holdings.lei, 
+                             legal_entities.name,
+                             legal_entities.title,
+                             holdings.isin,
+                             holdings.cusip,
+                             holdings.units,
+                             holdings.balance,
+                             holdings.val_usd
+                       FROM holdings
+                       INNER JOIN legal_entities ON holdings.lei = legal_entities.lei 
+                       WHERE held_by = %s
+                       ORDER BY val_usd DESC
+                       LIMIT 10;'''

a635de89771c204f49c5f76f6cb900df26b2c5b2
Jason Wu <jason.wu2153@gmail.com>
2020-12-08 23:39:10 -0500

    added pie chart holdings sql

--- sql.py
+++ sql.py
@@ -13,15 +13,23 @@ get_top_ten_holdings_by_value = '''SELECT holdings.lei,
                                     ORDER BY val_usd DESC
                                     LIMIT 10;'''
 get_top_ten_holdings_by_number_shares = '''SELECT holdings.lei, 
-                                          legal_entities.name,
-                                          legal_entities.title,
-                                          holdings.isin,
-                                          holdings.cusip,
-                                          holdings.units,
-                                          holdings.balance,
-                                          holdings.val_usd
-                                    FROM holdings
-                                    INNER JOIN legal_entities ON holdings.lei = legal_entities.lei 
-                                    WHERE held_by = %s AND units = 'NS'
-                                    ORDER BY balance DESC
-                                    LIMIT 10;'''
+                                                  legal_entities.name,
+                                                  legal_entities.title,
+                                                  holdings.isin,
+                                                  holdings.cusip,
+                                                  holdings.units,
+                                                  holdings.balance,
+                                                  holdings.val_usd
+                                            FROM holdings
+                                            INNER JOIN legal_entities ON holdings.lei = legal_entities.lei 
+                                            WHERE held_by = %s AND units = 'NS'
+                                            ORDER BY balance DESC
+                                            LIMIT 10;'''
+
+get_pie_chart_holdings_data = '''SELECT legal_entities.name,
+                                        holdings.balance,
+                                        holdings.val_usd
+                                 FROM holdings
+                                 INNER JOIN legal_entities ON holdings.lei = legal_entities.lei
+                                 WHERE held_by = %s AND units = 'NS'
+                                 ORDER BY val_usd DESC;'''

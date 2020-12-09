a34edd5a50bba792836509a121272dcd95e5e0c3
Jason Wu <jason.wu2153@gmail.com>
2020-12-09 17:31:42 -0500

    changed top 10 to top 5

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from db import cnx
-from sql import get_sec_companies_sql, get_top_ten_holdings_by_value, get_top_ten_holdings_by_number_shares, get_pie_chart_holdings_data
+from sql import get_sec_companies_sql, get_top_five_holdings_by_value, get_top_five_holdings_by_number_shares, get_pie_chart_holdings_data
 
 app = Flask(__name__)
 CORS(app)
@@ -27,18 +27,18 @@ def get_holdings_data():
     cik = request.args.get('cik')
     cursor = cnx.cursor()
 
-    # top ten by value
-    cursor.execute(get_top_ten_holdings_by_value, (cik,))
+    # top 5 by value
+    cursor.execute(get_top_five_holdings_by_value, (cik,))
     desc = cursor.description
     attr_names = [attr[0] for attr in desc]
-    top_ten_by_value_data = [dict(zip(attr_names, row))  
+    top_five_by_value_data = [dict(zip(attr_names, row))  
         for row in cursor.fetchall()]
 
-    # top ten by number of shares
-    cursor.execute(get_top_ten_holdings_by_number_shares, (cik,))
+    # top 5 by number of shares
+    cursor.execute(get_top_five_holdings_by_number_shares, (cik,))
     desc = cursor.description
     attr_names = [attr[0] for attr in desc]
-    top_ten_by_number_shares_data = [dict(zip(attr_names, row))  
+    top_five_by_number_shares_data = [dict(zip(attr_names, row))  
         for row in cursor.fetchall()]
 
     # pie chart data
@@ -49,8 +49,8 @@ def get_holdings_data():
         for row in cursor.fetchall()]
 
     data = {
-        'top_ten_by_value': top_ten_by_value_data,
-        'top_ten_by_number_shares': top_ten_by_number_shares_data,
+        'top_five_by_value': top_ten_by_value_data,
+        'top_five_by_number_shares': top_ten_by_number_shares_data,
         'pie_chart_data': pie_chart_data
     }
 
--- sql.py
+++ sql.py
@@ -1,5 +1,5 @@
 get_sec_companies_sql = 'SELECT * FROM sec_companies ORDER BY name ASC;'
-get_top_ten_holdings_by_value = '''SELECT holdings.lei, 
+get_top_five_holdings_by_value = '''SELECT holdings.lei, 
                                           legal_entities.name,
                                           legal_entities.title,
                                           holdings.isin,
@@ -12,7 +12,7 @@ get_top_ten_holdings_by_value = '''SELECT holdings.lei,
                                     WHERE held_by = %s AND units = 'NS'
                                     ORDER BY val_usd DESC
                                     LIMIT 10;'''
-get_top_ten_holdings_by_number_shares = '''SELECT holdings.lei, 
+get_top_five_holdings_by_number_shares = '''SELECT holdings.lei, 
                                                   legal_entities.name,
                                                   legal_entities.title,
                                                   holdings.isin,
@@ -24,7 +24,7 @@ get_top_ten_holdings_by_number_shares = '''SELECT holdings.lei,
                                             INNER JOIN legal_entities ON holdings.lei = legal_entities.lei 
                                             WHERE held_by = %s AND units = 'NS'
                                             ORDER BY balance DESC
-                                            LIMIT 10;'''
+                                            LIMIT 5;'''
 get_pie_chart_holdings_data = '''SELECT legal_entities.name,
                                         holdings.balance,
                                         holdings.val_usd
